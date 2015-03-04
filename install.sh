c2c0dc91906b6d059ab0fb4ebd2004f53b5eb8f8
Korrow <Korrow@gmail.com>
2015-03-04 13:40:21 -0800

    Add support for zsh/oh-my-zsh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+backupdir="./backup"
+
+#######################################
+# Install oh-my-zsh
+######################################
+omz_dir="$HOME/.oh-my-zsh"
+omz_uri="git://github.com/robbyrussell/oh-my-zsh.git"
+
+if [ -d $omz_dir ]; then
+    echo "oh-my-zsh directory located, not re-cloning"
+else
+    git clone $omz_uri $omz_dir
+fi
+#######################################
+# Set up symbolic links to dotfiles.
+#######################################
+for file in dotfiles/*
+do
+    dest="${HOME}/.$(basename ${file})"
+    if [ -f $dest ]; then
+        bak_dest="${backupdir}/$(basename ${file})"
+        echo "File \"${dest}\" already exists, moving to \"${bak_dest}\""
+        mv $dest $bak_dest
+    fi
+    echo "Linking from \"${file}\" to \"${dest}\""
+    ln -sr ${file} ${dest}
+done
+
