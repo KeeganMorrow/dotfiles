65282a3a4f977268264478ed64ca2471f4a80b36
Korrow <Korrow@gmail.com>
2015-03-05 00:48:00 -0800

    Zsh: Remove a bunch of the author's customizations
    Removed things like aliases, changes to the PATH, etc from
    the zsh config.

--- dotfiles/zsh/exports.zsh
+++ dotfiles/zsh/exports.zsh
@@ -1,5 +1,4 @@
-# Currently this path is appendend to dynamically when picking a ruby version
-export PATH=bin:script:~/.rbenv/bin:~/.bin:node_modules/.bin:/usr/local/sbin:/usr/local/bin:/usr/local/share/npm/bin:~/.cabal/bin:$HOME/Library/Haskell/bin:$PATH
+export PATH"=/home/keegan/bin:/usr/local/bin:/usr/sbin:/sbin:/home/keegan/bin:/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/games"
 
 # Setup terminal, and turn on colors
 export TERM=xterm-256color
@@ -10,13 +9,3 @@ export LSCOLORS=Gxfxcxdxbxegedabagacad
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 
-# This resolves issues install the mysql, postgres, and other gems with native non universal binary extensions
-export ARCHFLAGS='-arch x86_64'
-
-export LESS='--ignore-case --raw-control-chars'
-export PAGER='most'
-export NODE_PATH=/opt/boxen/nodenv/versions/v0.10/lib
-export EDITOR='nvim'
-export PYTHONPATH=/usr/local/lib/python2.6/site-packages
-# CTAGS Sorting in VIM/Emacs is better behaved with this in place
-export LC_COLLATE=C
