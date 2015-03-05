79b7b5fd7435ae1d12dd91d64ea518e5924fdd14
Korrow <Korrow@gmail.com>
2015-03-05 02:14:37 -0800

    Add antigen for zsh
    Installed antigen and removed zsh_hooks (they were incompatible with
    the oh-my-zsh libraries).

---
+++ dotfiles/zsh/plugins.zsh
@@ -0,0 +1,19 @@
+source ${HOME}/syncsettings/repos/antigen/antigen.zsh
+
+# Load the oh-my-zsh's library
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+#antigen bundle gitfast
+#antigen bundle pip
+#antigen bundle sublime
+#antigen bundle command-not-found
+
+# Syntax highlighting bundle.
+#antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+#antigen theme robbyrussell
+
+# Tell antigen that you're done.
+antigen apply
