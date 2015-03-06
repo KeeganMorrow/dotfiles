4c19a4cc5a2cedeab68ad31ae44d3aa9a06520c7
Korrow <Korrow@gmail.com>
2015-03-05 20:28:07 -0800

    Set up zsh prompt theming

--- dotfiles/zsh/plugins.zsh
+++ dotfiles/zsh/plugins.zsh
@@ -4,16 +4,16 @@ source ${HOME}/syncsettings/repos/antigen/antigen.zsh
 antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
-#antigen bundle gitfast
-#antigen bundle pip
-#antigen bundle sublime
-#antigen bundle command-not-found
+antigen bundle gitfast
+antigen bundle pip
+antigen bundle sublime
+antigen bundle command-not-found
 
 # Syntax highlighting bundle.
-#antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-syntax-highlighting
 
 # Load the theme.
-#antigen theme robbyrussell
+antigen theme blinks
 
 # Tell antigen that you're done.
 antigen apply
