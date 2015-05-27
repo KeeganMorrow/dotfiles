9f6d32f8ba8f42e07e3084448f62c58061c43263
Keegan Morrow <Korrow@gmail.com>
2015-05-27 15:16:59 -0700

    nose: tweak color/terseout handling

--- dotfiles/zsh/exports.zsh
+++ dotfiles/zsh/exports.zsh
@@ -1,5 +1,3 @@
 export EDITOR=vim
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/games:/opt/kde3/bin"
-# Enable colored output for Python Nose
-export NOSE_REDNOSE=1
 
