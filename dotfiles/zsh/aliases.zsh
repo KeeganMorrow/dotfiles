ba47d97500d8f110d54e08b21ccdff83da3fc522
Korrow <Korrow@gmail.com>
2015-06-25 09:32:53 -0700

    zsh: Add aliases for virtualenv commands

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -1,2 +1,15 @@
+# Always use colordiff
 alias diff=colordiff
+
+# Aliases for virtualenvwrapper
+# Taken from http://mrcoles.com/tips-using-pip-virtualenv-virtualenvwrapper/
+alias v='workon'
+alias v.deactivate='deactivate'
+alias v.mk='mkvirtualenv'
+alias v.rm='rmvirtualenv'
+alias v.switch='workon'
+alias v.add2virtualenv='add2virtualenv'
+alias v.cdsitepackages='cdsitepackages'
+alias v.cd='cdvirtualenv'
+alias v.lssitepackages='lssitepackages'
 
