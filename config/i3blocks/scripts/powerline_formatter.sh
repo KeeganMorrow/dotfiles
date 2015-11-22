c6c81f14c6c20bcd4f0567ec374e5c3f9ce1a550
Keegan Morrow <Korrow@gmail.com>
2015-11-22 15:55:40 -0800

    i3: remove i3blocks config
    Remove i3blocks config since now using i3pystatus

--- config/i3blocks/scripts/powerline_formatter.sh
+++
@@ -1,9 +0,0 @@
-#/bin/sh
-
-BACKGROUND_BLUE=
-BACKGROUND_GREEN=
-FOREGROUND="#181818"
-
-POWERLINE_PREFIX="<span color='${FOREGROUND}' background='${BACKGROUND}'>"
-POWERLINE_SUFFIX="</span><span color='${BACKGROUND}' background='${FOREGROUND}'></span>"
-
