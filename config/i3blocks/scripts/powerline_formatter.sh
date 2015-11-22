25d46008cc3092e07fc3252abebbcb7715657661
Keegan Morrow <Korrow@gmail.com>
2015-11-22 15:54:22 -0800

    i3blocks: Add powerline formatting script
    Add powerline formatting script for i3blocks. Even though I've decided
    to go with i3pystatus for now, this may still be useful later.

---
+++ config/i3blocks/scripts/powerline_formatter.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+BACKGROUND_BLUE=
+BACKGROUND_GREEN=
+FOREGROUND="#181818"
+
+POWERLINE_PREFIX="<span color='${FOREGROUND}' background='${BACKGROUND}'>"
+POWERLINE_SUFFIX="</span><span color='${BACKGROUND}' background='${FOREGROUND}'></span>"
+
