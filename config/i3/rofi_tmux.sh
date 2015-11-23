72a5f7a1a3ae24b125d6aa5f44f8ea99f7f22304
Keegan Morrow <Korrow@gmail.com>
2015-11-22 21:11:25 -0800

    i3: disable tmux selector if no tmux

--- config/i3/rofi_tmux.sh
+++ config/i3/rofi_tmux.sh
@@ -3,7 +3,16 @@
 options="option1\noption2\noption3"
 
 windows="$(tmux ls)"
+if [ ! "$?" -eq "0" ] ; then
+    echo "tmux returned non-zero exit code"
+    exit 1
+fi
+
 choice=$(echo -e "$windows" | rofi -dmenu)
 target=$(echo -e "$choice" | sed 's/:.*//')
-urxvt-256color --hold -e zsh -c "tmux attach -t ${target}" &
+if [ ! -z "$target" ] ; then
+    urxvt-256color --hold -e zsh -c "tmux attach -t ${target}" &
+else
+    echo "No user input selected!"
+fi
 
