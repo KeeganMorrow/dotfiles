dd278e56264bd5f5b26cab032975189ad1ac5ea1
Keegan Morrow <Korrow@gmail.com>
2015-11-24 00:34:39 -0800

    bspwm: Add basic panel python script
    Requires xft fork of lemonbar

--- config/bspwm/bspwm_wrapper.sh
+++ config/bspwm/bspwm_wrapper.sh
@@ -4,15 +4,21 @@
 # Socket to use for communicating with bspwm, also in dotfiles/zsh/exports.zsh
 export BSPWM_SOCKET="/tmp/bspwm-socket.${USER}"
 
+export LEMONBAR_FONT="Source Code Pro:size=9px"
+export LEMONBAR_ARGS="-f ${LEMONBAR_FONT}"
 while :
 do
     echo "Running bspwm"
     echo "starting bspwm"
     bspwm &
+    bspwmpid=$!
+    echo "starting panel"
+    python3 ${HOME}/syncsettings/config/bspwm/panel.py | lemonbar $LEMONBAR_ARGS &
+    panelpid=$!
     # Block on sxhkd
     echo "starting shxkd"
     sxhkd
     echo "sxhkd exited, killing remaining..."
-    killall bspwm sxhkd
+    kill $bspwmpid $panelpid
     echo "bspwm session exited"
 done
---
+++ config/bspwm/panel.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import subprocess
+import re
+import datetime
+import sys
+
+BACKGROUND_COLOR="#080808"
+
+def powerlinify(inputstring, color, type=None, bgcolor=BACKGROUND_COLOR):
+    prefix='%{{F{2}}}%{{B{1}}}'
+    arrow = "%{{R}}{0}%{{R}}"
+    suffix = "%{{F-}}%{{B-}}"
+    if type == 'left':
+        arrowchar = ''
+        prefix += arrow
+    elif type == 'right':
+        arrowchar = ''
+        suffix = arrow + suffix
+    else:
+        arrowchar = ''
+    prefix = prefix.format(arrowchar, color, bgcolor)
+    suffix = suffix.format(arrowchar, color, bgcolor)
+    return prefix + inputstring + suffix
+
+class TimeIndicator():
+    def run(self):
+        return time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime())
+
+class DesktopInfo():
+    def __init__(self):
+        pass
+
+    def run(self):
+        result = ''
+        pattern = re.compile(b':([O,o,F,f])(\d)')
+        status = subprocess.check_output(['bspc', 'control', '--get-status'])
+        for (letter, num) in re.findall(pattern, status):
+            if letter.isupper():
+                # The bakground color thing isn't nice...
+                result += ' %{{F#FFFFFF}}{0}%{{F{1}}} '.format(num, BACKGROUND_COLOR)
+            else:
+                result += ' {0} '.format(num)
+        result += ''
+        return result
+
+timeindicator = TimeIndicator()
+desktopinfo = DesktopInfo()
+while (True):
+    statusline = '%{l}'
+    statusline += powerlinify(desktopinfo.run(), '#00afff', 'right')
+    statusline += '%{r}'
+    statusline += powerlinify(timeindicator.run(), '#afff00', 'left')
+    print(statusline)
+    sys.stdout.flush()
+    time.sleep(0.25)
+
