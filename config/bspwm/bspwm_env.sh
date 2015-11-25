d34c3cb9cc56b26c9f7b317f8ed0c4b7d9333433
Keegan Morrow <Korrow@gmail.com>
2015-11-24 22:22:43 -0800

    bspwm: Move bspwm environment into seperate script
    May want to have zsh source this file?

---
+++ config/bspwm/bspwm_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Socket to use for communicating with bspwm, also in dotfiles/zsh/exports.zsh
+export BSPWM_SOCKET="/tmp/bspwm-socket.${USER}"
+
+export LEMONBAR_FONT="Source Code Pro for Powerline:size=9px"
--- config/bspwm/bspwm_wrapper.sh
+++ config/bspwm/bspwm_wrapper.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-
-# If this grows beyond more than like one, make a seperate common file
-# Socket to use for communicating with bspwm, also in dotfiles/zsh/exports.zsh
-export BSPWM_SOCKET="/tmp/bspwm-socket.${USER}"
-
-export LEMONBAR_FONT="Source Code Pro for Powerline:size=9px"
-export LEMONBAR_ARGS="-f '${LEMONBAR_FONT}'"
+source $(dirname $0)/bspwm_env.sh
 while :
 do
     echo "Running bspwm"
@@ -13,7 +7,7 @@ do
     bspwm &
     bspwmpid=$!
     echo "starting panel"
-    python3 ${HOME}/syncsettings/config/bspwm/panel.py | lemonbar ${LEMONBAR_ARGS} &
+    python3 ${HOME}/syncsettings/config/bspwm/panel.py | lemonbar -f "${LEMONBAR_FONT}" &
     panelpid=$!
     # Block on sxhkd
     echo "starting shxkd"
