01ec4da511c39f450e1b6917977b05be74b0755d
Korrow <Korrow@gmail.com>
2016-01-22 16:20:43 -0800

    zsh: Switch from zgen to zplug
    Trying another zsh plugin manager.

--- dotfiles/zsh/plugins.zsh
+++ dotfiles/zsh/plugins.zsh
@@ -1,28 +1,30 @@
-source ${HOME}/syncsettings/repos/zgen/zgen.zsh
+source ${HOME}/syncsettings/repos/zplug/zplug
 
-# check if there's no init script
-if ! zgen saved; then
-    echo "Creating a zgen save"
+# Oh-My-Zsh plugins
+zplug "plugins/gitfast",                from:oh-my-zsh
+zplug "plugins/pip",                    from:oh-my-zsh
+zplug "plugins/sublime",                from:oh-my-zsh
+zplug "plugins/command-not-found",      from:oh-my-zsh
+zplug "plugins/ssh-agent",              from:oh-my-zsh
+# Other Plugins
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "willghatch/zsh-cdr"
+# Should be loaded after zsh-cdr
+zplug "zsh-users/zaw"
 
-    # Load oh-my-zsh
-    zgen oh-my-zsh
+# Themes
+zplug "korrow/zsh-themes", of:"themes/keegs.zsh-theme"
 
-    # Oh-My-Zsh plugins
-    zgen oh-my-zsh plugins/gitfast
-    zgen oh-my-zsh plugins/pip
-    zgen oh-my-zsh plugins/sublime
-    zgen oh-my-zsh plugins/command-not-found
-    zgen oh-my-zsh plugins/ssh-agent
-    # Other Plugins
-    zgen load zsh-users/zsh-syntax-highlighting
 
-    # Themes
-    zgen oh-my-zsh themes/arrow
-    zgen load korrow/zsh-themes themes/keegs
-
-    # Save all to init script
-    zgen save
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
 fi
 
+# Then, source plugins and add commands to $PATH
+zplug load --verbose
 # Enable ssh agent forwarding support
 zstyle :omz:plugins:ssh-agent agent-forwarding on
