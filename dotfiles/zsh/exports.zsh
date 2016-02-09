1b2b78853300e5647fd05377b6e9a3b86a58dfc9
Korrow <Korrow@gmail.com>
2016-02-09 14:32:01 -0800

    Add home bin to path

--- dotfiles/zsh/exports.zsh
+++ dotfiles/zsh/exports.zsh
@@ -1,5 +1,5 @@
 export EDITOR=vim
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/games:/opt/kde3/bin"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/bin/X11:/usr/sbin:/sbin:/usr/X11R6/bin:/usr/games:/opt/kde3/bin:${HOME}/bin/bin"
 
 # Socket to use for communicating with bspwm, also in config/bspwm/bspwm_wrapper.sh
 export BSPWM_SOCKET="/tmp/bspwm-socket.${USER}"
