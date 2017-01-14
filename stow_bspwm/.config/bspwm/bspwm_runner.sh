9aa49f54a9766553d9ced2a3e9ae16191469313b
Keegan Morrow <Korrow@gmail.com>
2017-01-14 09:27:27 -0800

    Bspwm: Start panel once again

--- stow_bspwm/.config/bspwm/bspwm_runner.sh
+++ stow_bspwm/.config/bspwm/bspwm_runner.sh
@@ -4,10 +4,14 @@ echo "Starting bspwm"
 bspwm &
 bspwmpid=$!
 
+echo "Starting panel"
+python3 ${HOME}/syncsettings/repos/lemonaconda/example.py | lemonbar -f "${LEMONBAR_FONT}" -f ${LEMONBAR_FONT_SYMS} -g "${LEMONBAR_GEOMETRY} -B ${LEMONBAR_COLOR}" &
+panelpid=$!
+
 # Block on sxhkd
 echo "Starting shxkd"
 sxhkd
 
 echo "sxhkd exited, killing remaining..."
-kill $bspwmpid
+kill $bspwmpid $panelpid
 echo "Exiting bspwm wrapper script"
