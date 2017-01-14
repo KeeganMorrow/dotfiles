a42546a0d9d63e086f6a7e8a90ee176391cdff05
Keegan Morrow <Korrow@gmail.com>
2017-01-14 09:27:45 -0800

    Bspwm: Set background using nitrogen

--- stow_bspwm/.config/bspwm/bspwm_runner.sh
+++ stow_bspwm/.config/bspwm/bspwm_runner.sh
@@ -8,6 +8,9 @@ echo "Starting panel"
 python3 ${HOME}/syncsettings/repos/lemonaconda/example.py | lemonbar -f "${LEMONBAR_FONT}" -f ${LEMONBAR_FONT_SYMS} -g "${LEMONBAR_GEOMETRY} -B ${LEMONBAR_COLOR}" &
 panelpid=$!
 
+echo "Setting background"
+nitrogen --restore &
+
 # Block on sxhkd
 echo "Starting shxkd"
 sxhkd
