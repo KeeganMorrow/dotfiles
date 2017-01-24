09f5b072359ed281db40e49eb08ae797e8d0975e
Keegan Morrow <Korrow@gmail.com>
2017-01-23 16:04:06 -0800

    Update BSPWM/SXHKD config

--- stow_bspwm/.config/bspwm/bspwm_wrapper.sh
+++ stow_bspwm/.config/bspwm/bspwm_wrapper.sh
@@ -2,14 +2,15 @@
 
 # Used to check if certain things should be restarted
 export BSPWM_FIRST_RUN="1"
+export BSPWM_WRAPPER_LOG="/home/keegan/bspwm_log.log"
 
 while :
 do
-    echo "Entering bspwm wrapper script loop"
-    echo "Sourcing bspwm environment"
-    source $(dirname $0)/bspwm_env.sh
+    echo "Entering bspwm wrapper script loop" >> $BSPWM_WRAPPER_LOG
+    echo "Sourcing bspwm environment" >> $BSPWM_WRAPPER_LOG
+    . $(dirname $0)/bspwm_env.sh 2>&1 >> $BSPWM_WRAPPER_LOG
     echo "Entering bspwm runner script"
     $(dirname $0)/bspwm_runner.sh 2>&1 >> $BSPWM_WRAPPER_LOG
-    echo "Completed an iteration of the loop. Hoorah!"
+    echo "Completed an iteration of the loop. Hoorah!" >> $BSPWM_WRAPPER_LOG
     export BSPWM_FIRST_RUN="0"
 done
