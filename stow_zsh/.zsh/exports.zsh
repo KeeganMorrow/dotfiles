9130a0698c0178a69d7a43b75d8060e0f083b8f2
Keegan Morrow <Korrow@gmail.com>
2017-02-04 19:51:39 -0800

    Zsh: Use rg with FZF

--- stow_zsh/.zsh/exports.zsh
+++ stow_zsh/.zsh/exports.zsh
@@ -3,3 +3,6 @@ export PATH="$PATH:${HOME}/bin/bin:${HOME}/syncsettings/bin"
 
 # Socket to use for communicating with bspwm, also in config/bspwm/bspwm_wrapper.sh
 export BSPWM_SOCKET="/tmp/bspwm-socket.${USER}"
+
+# Use rg with fzf
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
