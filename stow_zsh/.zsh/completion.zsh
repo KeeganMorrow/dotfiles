39dba8eb48dd3b41b2ce8d9ec48a34a9a78493e4
Korrow <Korrow@gmail.com>
2017-02-07 14:33:17 -0800

    Zsh: Rework completion setup

--- stow_zsh/.zsh/completion.zsh
+++ stow_zsh/.zsh/completion.zsh
@@ -1,8 +1,88 @@
-# add in zsh-completions
-fpath=(/opt/boxen/homebrew/share/zsh-completions $fpath)
-
 autoload -U compinit && compinit
 zmodload -i zsh/complist
+################################################################
+# Completion options
+################################################################
+################################
+# setopt completion options
+################################
+
+# Bring up completion menu on abiguous completion
+setopt menu_complete 
+
+# Unset as to not to collide with menu_complete
+unsetopt auto_menu
+
+# Keep the cursor in place until a completion
+setopt complete_in_word
+
+# Move the cursor to end after completion
+setopt always_to_end
+
+# Add a trailing slash instead of a space
+setopt auto_param_slash
+
+# List files like "ls -F"
+setopt list_types
+
+# Open completion on globs
+setopt glob_complete
+
+# Do not require a leading `.' in filename to be matched
+setopt glob_dots
+
+################################
+# zstyle completion options
+################################
+
+# Enable completion caching, use rehash to clear
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
+
+# match uppercase from lowercase, and left-side substrings
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}' '+l:|=*'
+
+# Activate menu selection
+zstyle ':completion:*' menu select
+
+# Fuzzy match mistyped completions.
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+ 
+################################################################
+# Formatting and Messages                                      #
+################################################################
+
+# More verbose completion, including descriptions for command options
+zstyle ':completion:*' verbose yes
+
+# Show command short descriptions
+zstyle ':completion:*' extra-verbose yes
+
+# Group completion matches
+zstyle ':completion:*' group-name ''
+
+# Add simple colors to kill
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+
+################################
+# Messages                     #
+################################
+# format all messages not formatted in bold prefixed with ----
+zstyle ':completion:*' format "%F{214}%B-- %d --%b%f"
+# format descriptions
+zstyle ':completion:*:descriptions' format $"$fg[green]completing %B%d%b%"
+# bold and underline normal messages
+zstyle ':completion:*:messages' format '%B%U---- %d%u%b'
+# format in bold red error messages
+zstyle ':completion:*:warnings' format "%B$fg[red]%}---- No match for: $fg[white]%d%b"
+
+
+################################################################
+# Additional completions
+################################################################
+
 #----------------------------------------------------------------------
 # Disable usernames in tilde completion
 # Taken from http://superuser.com/questions/344128/disable-users-from-completing-after-a-in-zsh
@@ -26,64 +106,3 @@ _bangfalse_tilde () {
   (( ret )) || return 0
 }
 compdef _bangfalse_tilde -tilde-
-
-
-# man zshcontrib
-zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{5}]%f '
-zstyle ':vcs_info:*' enable git #svn cvs 
-
-# Enable completion caching, use rehash to clear
-zstyle ':completion::complete:*' use-cache on
-zstyle ':completion::complete:*' cache-path ~/.zsh/cache/$HOST
-
-# Fallback to built in ls colors
-zstyle ':completion:*' list-colors ''
-
-# Make the list prompt friendly
-zstyle ':completion:*' list-prompt '%SAt %p: Hit TAB for more, or the character to insert%s'
-
-# Make the selection prompt friendly when there are a lot of choices
-zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
-
-# Add simple colors to kill
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
-
-# list of completers to use
-zstyle ':completion:*::::' completer _expand _complete _ignored _approximate
-
-zstyle ':completion:*' menu select=1 _complete _ignored _approximate
-
-# insert all expansions for expand completer
-# zstyle ':completion:*:expand:*' tag-order all-expansions
- 
-# match uppercase from lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
- 
-# offer indexes before parameters in subscripts
-zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
-
-# formatting and messages
-zstyle ':completion:*' verbose yes
-zstyle ':completion:*:descriptions' format '%B%d%b'
-zstyle ':completion:*:messages' format '%d'
-zstyle ':completion:*:warnings' format 'No matches for: %d'
-zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
-zstyle ':completion:*' group-name ''
- 
-# ignore completion functions (until the _ignored completer)
-zstyle ':completion:*:functions' ignored-patterns '_*'
-zstyle ':completion:*:scp:*' tag-order files users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
-zstyle ':completion:*:scp:*' group-order files all-files users hosts-domain hosts-host hosts-ipaddr
-zstyle ':completion:*:ssh:*' tag-order users 'hosts:-host hosts:-domain:domain hosts:-ipaddr"IP\ Address *'
-zstyle ':completion:*:ssh:*' group-order hosts-domain hosts-host users hosts-ipaddr
-zstyle '*' single-ignored show
-
-
-# ZAW styles
-zstyle ':filter-select:highlight' matched fg=yellow,standout
-zstyle ':filter-select' max-lines 10 # use 10 lines for filter-select
-zstyle ':filter-select' max-lines -10 # use $LINES - 10 for filter-select
-zstyle ':filter-select' rotate-list yes # enable rotation for filter-select
-zstyle ':filter-select' case-insensitive yes # enable case-insensitive search
-zstyle ':filter-select' extended-search no # see below
