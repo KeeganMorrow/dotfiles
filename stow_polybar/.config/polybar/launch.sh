a4bad3095ab0eb383b396e96db002fd4ae0effa3
Keegan Morrow <Korrow@gmail.com>
2017-05-01 10:18:25 -0700

    Add polybar configuration

---
+++ stow_polybar/.config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+polybar example &
+
+# Launch bar1 and bar2
+# polybar bar1 &
+# polybar bar2 &
+
+echo "Bars launched..."
