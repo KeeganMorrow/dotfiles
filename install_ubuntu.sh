a47c1caeba9bbcac4bb8b178cad9fcc29e49ec4a
Korrow <Korrow@gmail.com>
2018-09-23 22:37:32 -0700

    Add alacritty to Ubuntu install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -25,29 +25,41 @@ EOM
 
 echo "Installing Ubuntu packages"
 for i in $ubuntu_packages ; do
-	sudo apt install -y "$i"
+    sudo apt install -y "$i"
 done
 
 echo "Installing Python 3 packages"
 for i in $python3_packages ; do
-	sudo -H pip3 install "$i"
+    sudo -H pip3 install "$i"
 done
 
 if hash rg 2>/dev/null ; then
-	echo "Ripgrep already installed"
+    echo "Ripgrep already installed"
 else
-	echo "Installing ripgrep"
-	pushd builddir
-	ripgrep_version="0.10.0"
-	curl -LO "https://github.com/BurntSushi/ripgrep/releases/download/${ripgrep_version}/ripgrep_${ripgrep_version}_amd64.deb"
-	sudo dpkg -i "ripgrep_${ripgrep_version}_amd64.deb"
-	popd
+    echo "Installing ripgrep"
+    pushd builddir
+    ripgrep_version="0.10.0"
+    curl -LO "https://github.com/BurntSushi/ripgrep/releases/download/${ripgrep_version}/ripgrep_${ripgrep_version}_amd64.deb"
+    sudo dpkg -i "ripgrep_${ripgrep_version}_amd64.deb"
+    popd
 fi
 
 if hash rustup 2>/dev/null; then
-	echo "Rust already installed"
+    echo "Rust already installed"
 else
-	echo "Installing Rust"
-	curl https://sh.rustup.rs -sSf | sh
+    echo "Installing Rust"
+    curl https://sh.rustup.rs -sSf | sh
 fi
 
+if hash alacritty 2>dev/null; then
+    echo "Alacritty already installed"
+else
+    echo "Installing Alacritty"
+    pushd builddir
+    git clone https://github.com/jwilm/alacritty.git
+    cd alacritty
+    git checkout v0.2.0
+    cargo install cargo-deb
+    cargo deb --install
+    popd
+fi
