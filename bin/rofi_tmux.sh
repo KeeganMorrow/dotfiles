996b89e271d7ddf046f845d41a976ed0f2c5add7
Keegan Morrow <Korrow@gmail.com>
2020-07-23 22:04:45 -0700

    rofi_tmux: Various fixups

--- bin/rofi_tmux.sh
+++ bin/rofi_tmux.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-options="option1\noption2\noption3"
-
 if [ -z "$TERMINAL" ] ; then
-    term="st"
+    term="alacritty"
 else
     term="$TERMINAL"
 fi
 
 windows="$(tmux ls)"
-if [ ! "$?" -eq "0" ] ; then
+if [ "$?" -ne "0" ] ; then
     echo "tmux returned non-zero exit code"
     exit 1
 fi
@@ -20,7 +18,7 @@ detached_windows="$(echo -e "$windows" | awk '!/attached/')"
 
 choice=$(echo -e "$detached_windows \n$attached_windows" | rofi -dmenu $@)
 target=$(echo -e "$choice" | sed 's/:.*//')
-if [ ! -z "$target" ] ; then
+if [ -n "$target" ] ; then
     eval "$term -e zsh -c \"tmux attach -t '${target}'\" &"
 else
     echo "No user input selected!"
