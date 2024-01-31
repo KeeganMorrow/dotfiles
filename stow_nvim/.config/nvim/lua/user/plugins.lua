ee57211a02ce5c0e0bd881296c53b545dcbec094
Keegan Morrow <Korrow@gmail.com>
2024-01-31 15:30:45 -0800

    neovim: Remove vsnip
    Was causing issues, remove for now at least

--- stow_nvim/.config/nvim/lua/user/plugins.lua
+++ stow_nvim/.config/nvim/lua/user/plugins.lua
@@ -753,14 +753,12 @@ require("lazy").setup({
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            "hrsh7th/vim-vsnip",
             "hrsh7th/cmp-buffer",
             "f3fora/cmp-spell",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-calc",
-            "hrsh7th/cmp-vsnip",
             "onsails/lspkind-nvim",
             "petertriho/cmp-git",
         },
@@ -787,11 +785,6 @@ require("lazy").setup({
 
             local cmp = require("cmp")
             cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body)
-                    end,
-                },
                 formatting = {
                     format = require("lspkind").cmp_format({
                         mode = "symbol_text",
@@ -816,23 +809,18 @@ require("lazy").setup({
                         select = false,
                     }),
                     ["<Tab>"] = cmp.mapping(function(fallback)
+                        -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
                         if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif vim.fn["vsnip#available"](1) == 1 then
-                            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                        elseif has_words_before() then
-                            cmp.complete()
+                            local entry = cmp.get_selected_entry()
+                            if not entry then
+                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                            else
+                                cmp.confirm()
+                            end
                         else
-                            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+                            fallback()
                         end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function()
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                            feedkey("<Plug>(vsnip-jump-prev)", "")
-                        end
-                    end, { "i", "s" }),
+                    end, { "i", "s", "c" }),
                 },
                 sources = {
                     { name = "nvim_lsp" },
@@ -840,7 +828,6 @@ require("lazy").setup({
                     { name = "spell" },
                     { name = "path" },
                     { name = "calc" },
-                    { name = "vsnip" },
                     { name = "buffer" },
                     { name = "git" },
                 },
@@ -918,11 +905,6 @@ require("lazy").setup({
             })
         end,
     },
-
-    --    -- Snippets
-    { "hrsh7th/vim-vsnip" },
-    { "hrsh7th/vim-vsnip-integ" },
-    { "rafamadriz/friendly-snippets" },
 
     -- Other plugins
     {
