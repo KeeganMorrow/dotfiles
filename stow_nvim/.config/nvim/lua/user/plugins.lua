31603c270843f83f9244c860d48a44ac87d83fd6
Keegan Morrow <Korrow@gmail.com>
2024-02-15 11:45:40 -0800

    Mini: Don't lazy load
    Fixes trailspace

--- stow_nvim/.config/nvim/lua/user/plugins.lua
+++ stow_nvim/.config/nvim/lua/user/plugins.lua
@@ -158,14 +158,14 @@ require("lazy").setup({
     },
 
     {
-        "echasnovski/mini.nvim",
+        "echasnovski/mini.nvim", version= false, lazy=false,
         config = function()
             require("mini.trailspace").setup({
                 only_in_normal_buffers = true,
             })
         end,
         keys = {
-            { "<leader>W", "<cmd>MiniTrailspace.trim<cr>", desc = "Trim whitespace" },
+            { "<leader>W", "<CMD>lua MiniTrailspace.trim()<CR>", desc = "Trim whitespace" },
         },
     },
     {
