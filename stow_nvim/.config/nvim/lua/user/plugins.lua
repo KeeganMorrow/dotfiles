9964d4691843b9780de0f00b980f25f6a91930a4
Keegan Morrow <Korrow@gmail.com>
2025-07-09 10:17:06 -0700

    nvim: Minor updates
    Add interim-ls and switch to telescope project

--- stow_nvim/.config/nvim/lua/user/plugins.lua
+++ stow_nvim/.config/nvim/lua/user/plugins.lua
@@ -95,7 +95,10 @@ require("lazy").setup({
         "nvim-neorg/neorg",
         build = ":Neorg sync-parsers",
         -- tag = "*",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "benlubas/neorg-interim-ls",
+        },
         config = function()
             require("neorg").setup({
                 load = {
@@ -119,6 +122,34 @@ require("lazy").setup({
                             workspaces = {
                                 home = "~/neorg/home",
                                 work = "~/neorg/work",
+                            },
+                        },
+                    },
+                    ["external.interim-ls"] = {
+                        config = {
+                            -- default config shown
+                            completion_provider = {
+                                -- Enable or disable the completion provider
+                                enable = true,
+
+                                -- Show file contents as documentation when you complete a file name
+                                documentation = true,
+
+                                -- Try to complete categories provided by Neorg Query. Requires `benlubas/neorg-query`
+                                categories = false,
+
+                                -- suggest heading completions from the given file for `{@x|}` where `|` is your cursor
+                                -- and `x` is an alphanumeric character. `{@name}` expands to `[name]{:$/people:# name}`
+                                people = {
+                                    enable = false,
+
+                                    -- path to the file you're like to use with the `{@x` syntax, relative to the
+                                    -- workspace root, without the `.norg` at the end.
+                                    -- ie. `folder/people` results in searching `$/folder/people.norg` for headings.
+                                    -- Note that this will change with your workspace, so it fails silently if the file
+                                    -- doesn't exist
+                                    path = "people",
+                                },
                             },
                         },
                     },
@@ -763,10 +794,9 @@ require("lazy").setup({
                 end,
             },
             {
-                "ahmedkhalf/project.nvim",
+                "nvim-telescope/telescope-project.nvim",
                 config = function()
-                    require("project_nvim").setup({})
-                    require("telescope").load_extension("projects")
+                    require("telescope").load_extension("project")
                 end,
             },
         },
