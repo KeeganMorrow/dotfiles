ffdafee5bc2c7c53351bd644e1b74f2429cdc8ca
Keegan Morrow <Korrow@gmail.com>
2025-07-09 10:19:28 -0700

    nvim: Add Avante for experimenting

--- stow_nvim/.config/nvim/lua/user/plugins.lua
+++ stow_nvim/.config/nvim/lua/user/plugins.lua
@@ -90,7 +90,73 @@ require("lazy").setup({
     --------------------------------------------------------------------------------
     -- Functionality improvements
     --------------------------------------------------------------------------------
-
+    {
+        "yetone/avante.nvim",
+        -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+        -- ⚠️ must add this setting! ! !
+        build = "make BUILD_FROM_SOURCE=false",
+        event = "VeryLazy",
+        version = false, -- Never set this value to "*"! Never!
+        ---@module 'avante'
+        ---@type avante.Config
+        opts = {
+            -- add any opts here
+            -- for example
+            provider = "copilot",
+            providers = {
+                copilot = {
+                    endpoint = "https://api.githubcopilot.com",
+                    model = "gpt-4o-2024-11-20",
+                    proxy = nil, -- [protocol://]host[:port] Use this proxy
+                    allow_insecure = false, -- Allow insecure server connections
+                    timeout = 30000, -- Timeout in milliseconds
+                    context_window = 30000, -- Number of tokens to send to the model for context
+                    extra_request_body = {
+                        temperature = 0.75,
+                        max_tokens = 20480,
+                    },
+                },
+            },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            --- The below dependencies are optional,
+            "echasnovski/mini.pick", -- for file_selector provider mini.pick
+            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+            "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua", -- for file_selector provider fzf
+            "stevearc/dressing.nvim", -- for input provider dressing
+            "folke/snacks.nvim", -- for input provider snacks
+            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            {
+                -- support for image pasting
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    -- recommended settings
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        -- required for Windows users
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                -- Make sure to set this up properly if you have lazy=true
+                "MeanderingProgrammer/render-markdown.nvim",
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    },
     {
         "nvim-neorg/neorg",
         build = ":Neorg sync-parsers",
@@ -241,10 +307,29 @@ require("lazy").setup({
         "zbirenbaum/copilot.lua",
         config = function()
             require("copilot").setup({
-                suggestion = { enabled = false },
+                server = {
+                    type = "binary",
+                },
+                suggestion = {
+                    auto_trigger = true,
+                    enabled = true,
+                    keymap = {
+                        accept = "<Tab>",
+                    },
+                },
                 panel = { enabled = false },
             })
         end,
+        cmd = "Copilot",
+        event = "InsertEnter",
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+            filetypes = {
+                markdown = true,
+                help = true,
+            },
+        },
     },
     {
         "saghen/blink.cmp",
