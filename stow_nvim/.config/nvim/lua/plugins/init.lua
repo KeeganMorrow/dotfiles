a0d79caa71278093c3950c50563e40b57a55eebc
Keegan Morrow <Korrow@gmail.com>
2026-05-13 10:47:54 -0700

    nvim: add snacks.nvim

--- stow_nvim/.config/nvim/lua/plugins/init.lua
+++ stow_nvim/.config/nvim/lua/plugins/init.lua
@@ -1077,6 +1077,29 @@ return {
         end,
     },
     {
+      "folke/snacks.nvim",
+      priority = 1000,
+      lazy = false,
+      ---@type snacks.Config
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        explorer = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+        words = { enabled = true },
+      },
+    },
+    {
         "nvim-lualine/lualine.nvim",
         event = "VeryLazy", -- Load after VimEnter and other event-based plugins
         dependencies = { "nvim-tree/nvim-web-devicons" }, -- Ensure web-devicons is loaded for icons
