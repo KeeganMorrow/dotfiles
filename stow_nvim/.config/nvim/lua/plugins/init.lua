bf22259df4761241ec002902a99736eddb40d2d0
Keegan Morrow <Korrow@gmail.com>
2026-08-25 14:41:33 -0700

    nvim: use catppuccin mocha lualine theme

--- stow_nvim/.config/nvim/lua/plugins/init.lua
+++ stow_nvim/.config/nvim/lua/plugins/init.lua
@@ -857,7 +857,7 @@ return {
         config = function()
             require("lualine").setup({
                 options = {
-                    theme = "catppuccin",
+                    theme = "catppuccin-mocha",
                     icons_enabled = true,
                     component_separators = { left = "", right = "" },
                     section_separators = { left = "", right = "" },
